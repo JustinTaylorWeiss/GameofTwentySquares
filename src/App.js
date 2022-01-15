20d031510f2cac9fac2c094ce01c0614203428ac
unknown <justintaylorweiss@gmail.com>
2022-01-15 06:51:59 -0500

    router again

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import { Game }   from './game';
 function App() {
 
     return <Switch>
-        <Route path="/GameofTwentySquares" exact={true}>
-            <Redirect to="/GameofTwentySquares/game"/>
-        </Route>
-        <Route path="/GameofTwentySquares/game">
-            <Game/>
-        </Route>
-        <Route path="/" exact={true}>
-            <Redirect to="/game"/>
-        </Route>
-        <Route path="/game">
+        <Route path="/GameofTwentySquares">
             <Game/>
         </Route>
     </Switch>
 }
+
+/* 
+
+<Route path="/" exact={true}>
+    <Redirect to="/game"/>
+</Route>
+<Route path="/game">
+    <Game/>
+</Route>
+
+*/
 
 export default App;
